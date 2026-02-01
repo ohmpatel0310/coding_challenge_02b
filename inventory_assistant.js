fbffb4d6610ca7b2c6ede0ae181f8fc59eacd69e
Ohm Patel <ohmp3@usf.edu>
2026-02-01 14:44:11 -0500

    Calculated inventory metrics

--- inventory_assistant.js
+++ inventory_assistant.js
@@ -1,10 +1,16 @@
 // This is the inventory for the Brownie Shop
 
-let itemName = "Chocolate"
-let unitCost = 0.1
-let currentStock = 1000
-let reorderLevel = 300
-let targetStock = 1500
-let weeklyDemand = 700
-let supplierLeadTimeWeeks = 2
+let itemName = "Chocolate";
+let unitCost = 0.1;
+let currentStock = 1000;
+let reorderLevel = 300;
+let targetStock = 1500;
+let weeklyDemand = 700;
+let supplierLeadTimeWeeks = 2;
+
+let weeksOfCover = weeklyDemand > 0 currentStock / weeklyDemand : Infinity; 
+let stockDeficit = Math.max(0,targetStock - currentStock);
+let reorderQuantity = reorderNow ? Math.ceil(stockDeficit) : 0;
+let estimatedReorderCost = reorderQuantity * unitCost;
+let reorderNow = currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks;
 
