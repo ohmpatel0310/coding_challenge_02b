372d05f5491545a323ceb5766ed3ee22d435eaf2
Ohm Patel <ohmp3@usf.edu>
2026-02-01 14:58:40 -0500

    Fixed syntax errors

--- inventory_assistant.js
+++ inventory_assistant.js
@@ -8,9 +8,9 @@ let targetStock = 1500;
 let weeklyDemand = 700;
 let supplierLeadTimeWeeks = 2;
 
-let weeksOfCover = weeklyDemand > 0 currentStock / weeklyDemand : Infinity; 
-let stockDeficit = Math.max(0,targetStock - currentStock);
-let reorderQuantity = reorderNow ? Math.ceil(stockDeficit) : 0;
+let weeksOfCover = weeklyDemand > 0 ? currentStock / weeklyDemand : Infinity;
+let stockDeficit = Math.max(0, targetStock - currentStock);
+let reorderQuantity = (currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks) ? Math.ceil(stockDeficit) : 0;
 let estimatedReorderCost = reorderQuantity * unitCost;
 let reorderNow = currentStock <= reorderLevel || weeksOfCover < supplierLeadTimeWeeks;
 
@@ -18,4 +18,4 @@ console.log("Item Name:", itemName);
 console.log("Weeks of Cover:", weeksOfCover.toFixed(2));
 console.log("Recommended Reorder Quantity:", reorderQuantity);
 console.log("Estimated Reoder Cost: $" + estimatedReorderCost.toFixed(2));
-console.log("Reorder Now?" reorderNow);
+console.log("Reorder Now?", reorderNow);
