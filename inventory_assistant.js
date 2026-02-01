13754d7e0f133533612aa86fc3050674c2d08f0a
Ohm Patel <ohmp3@usf.edu>
2026-02-01 14:36:57 -0500

    Creates and Initialized Variables

---
+++ inventory_assistant.js
@@ -0,0 +1,10 @@
+// This is the inventory for the Brownie Shop
+
+let itemName = "Chocolate"
+let unitCost = 0.1
+let currentStock = 1000
+let reorderLevel = 300
+let targetStock = 1500
+let weeklyDemand = 700
+let supplierLeadTimeWeeks = 2
+
